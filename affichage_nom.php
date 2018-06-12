af8a77bdafa2875cf36ba41b3a1b7379102d5e89
Silvia Patono <silvia.patono@gmail.com>
2018-06-12 09:07:17 +0200

    Transformed in a real log in page

--- affichage_nom.php
+++ affichage_nom.php
@@ -2,7 +2,31 @@
 
 
 if (isset($_GET["nom"]) && isset($_GET["mdp"])) {
-	
-	echo $_GET["nom"];
-	echo $_GET["mdp"];
+
+	$nom = $_GET["nom"];
+	$mdp = $_GET["mdp"];
+	$usernames = array('Silvia', 'Quentino', 'Sami', 'Raph');
+	$password = array ('silvia', 'quentino', 'sami', 'raph');
+
+
+	if (in_array($nom, $usernames) && in_array($mdp, $password)){
+		$login = array_combine($usernames, $password);
+		foreach ($login as $key => $value) {
+			if ($value === $mdp && $key === $nom) {
+				header('Location: list_vip.php');
+			} 
+			else {
+				// echo "Invalid Username or password";
+				header('Location: authentication_page.php');
+			}
+		}	
+	}
+	 else {
+		echo "Invalid Username or password";
+	}
 }
+	
+	
+
+
+// verifier que le nom utilisatuer correspond à un nom existant. (on l'a dans un array). Si existe, il est renvoyé à notre backend, s'il est pas dedans, message d'erreur
