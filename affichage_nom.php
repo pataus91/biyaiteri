870a24e20b861bed088e779a9b4bc6bf73a2d15c
Silvia Patono <silvia.patono@gmail.com>
2018-06-13 19:09:14 +0200

    variable  renamed in

--- affichage_nom.php
+++ affichage_nom.php
@@ -2,15 +2,12 @@
 
 
 if (isset($_GET["nom"]) && isset($_GET["mdp"])) {
-
 	$nom = $_GET["nom"];
 	$mdp = $_GET["mdp"];
 	$usernames = array('Silvia', 'Quentino', 'Sami', 'Raph');
-	$password = array ('silvia', 'quentino', 'sami', 'raph');
-
-
-	if (in_array($nom, $usernames) && in_array($mdp, $password)){
-		$login = array_combine($usernames, $password);
+	$passwords = array ('silvia', 'quentino', 'sami', 'raph');
+	if (in_array($nom, $usernames) && in_array($mdp, $passwords)){
+		$login = array_combine($usernames, $passwords);
 		foreach ($login as $key => $value) {
 			if ($value === $mdp && $key === $nom) {
 				header('Location: list_vip.php');
@@ -25,8 +22,7 @@ if (isset($_GET["nom"]) && isset($_GET["mdp"])) {
 		echo "Invalid Username or password";
 	}
 }
-	
-	
+
 
 
 // verifier que le nom utilisatuer correspond à un nom existant. (on l'a dans un array). Si existe, il est renvoyé à notre backend, s'il est pas dedans, message d'erreur
